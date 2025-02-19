4f2870cb73b046246d38fe30e7cdc89aa3df786a
Rareura123 <asferreira.2004@gmail.com>
2025-02-19 17:05:08 +0000

    logjn

---
+++ projeto/front/src/components/login.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+//import {MDBContainer, MDBCol, MDBRow, MDBBtn, MDBIcon, MDBInput, MDBCheckbox } from 'mdb-react-ui-kit';
+<style scoped src='../css/login'></style>
+
+function Login() {
+  return (
+    <div>
+      <h2>Login</h2>
+      <p>Este é o componente de login.</p>
+    </div>
+  );
+}
+
+export default Login;
