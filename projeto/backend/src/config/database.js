842275df572dd6a31dcee2f7d777a57cf1f93822
Rodrigo Santos <157403302+RodrigoAI-eng@users.noreply.github.com>
2025-02-20 15:55:10 +0000

    BD corrigido

--- projeto/backend/src/config/database.js
+++ projeto/backend/src/config/database.js
@@ -1,17 +1,16 @@
+require('dotenv').config();
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'PINT',
-  password: 'R0dr3g02oo4',
-  port: 5432,
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
 });
 
-
 pool.connect()
-  .then(() => console.log('Conexão ao PostgreSQL estabelecida!'))
-  .catch(err => console.error('Erro ao conectar ao PostgreSQL:', err));
+  .then(() => console.log("Conexão ao PostgreSQL estabelecida!"))
+  .catch(err => console.error("Erro ao conectar ao PostgreSQL:", err));
 
 module.exports = pool;
-
