001156a1f29a7c89dc7e9e590d2585776653a0ad
Rodrigo Santos <157403302+RodrigoAI-eng@users.noreply.github.com>
2025-03-10 16:39:29 +0000

    sync.js alterado para criar os models

--- projeto/backend/src/config/sync.js
+++ projeto/backend/src/config/sync.js
@@ -1,8 +1,10 @@
 const sequelize = require("../config/db");
+const User = require("../database/models/User");
+const Cargo = require("../database/models/Cargo");
 
 (async () => {
   try {
-    await sequelize.sync({ alter: true }); // 🔹 Atualiza a estrutura sem perder dados
+    await sequelize.sync({ alter: true }); // Atualiza a estrutura sem perder dados
     console.log("Base de dados sincronizada!");
     process.exit();
   } catch (error) {
